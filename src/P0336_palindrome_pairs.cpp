a7a249f20c08eed666c35f03520343178708b591
Yao <yaozirui@outlook.com>
2022-09-17 23:13:33 +0800

    P0336 another temp process

--- src/P0336_palindrome_pairs.cpp
+++ src/P0336_palindrome_pairs.cpp
@@ -1,82 +1,57 @@
 #include <common_headers.h>
 
 class Solution {
-
-	bool static is_palindrome(const string& word, int left, int right)
-	{
-		while (left < right)
-			if (word[left++] != word[right++])
-				return false;
-
-		return true;
+	bool static isPalindrome(const string& str, int i, int j) {
+		while (i < j && str[i] == str[j]) ++i, --j;
+		return i >= j;
 	}
 
+	struct Trie {
+		Trie* next[26] = { nullptr };
+		int end_index = -1;
+		vector<int> palindromeIndexes;
 
-	class Trie {
-	public:
-#define ALPHABET_SIZE 26
-		Trie* next[ALPHABET_SIZE]{ nullptr };
-		vector<int> palin_word;
-		int word_end_index{ -1 };
-
-
-		Trie() = default;
-
-		void insert(const string& word, int word_index) {
-			Trie* current = this;
-			for (int i = word.size() - 1; i >= 0; --i)
-			{
-				int offset = word[i] - 'a';
-				if (current->next[offset] == nullptr)
-				{
-					current->next[offset] = new Trie;
-				}
-				if (is_palindrome(word, 0, i))
-				{
-					current->palin_word.emplace_back(word_index);
-				}
-				current = current->next[offset];
+		void insert(string& s, int i) {
+			Trie* node = this;
+			for (int j = s.size() - 1; j >= 0; --j) {
+				if (isPalindrome(s, 0, j)) node->palindromeIndexes.push_back(i); // A[i]'s prefix forms a palindrome
+				int c = s[j] - 'a';
+				if (!node->next[c]) node->next[c] = new Trie();
+				node = node->next[c];
 			}
-			current->palin_word.emplace_back(word_index);
-			current->word_end_index = word_index;
+			node->end_index = i;
+			node->palindromeIndexes.push_back(i); // A[i]'s prefix is empty string here, which is a palindrome.
 		}
+
 	};
+
+
 
 public:
 	vector<vector<int>> palindromePairs(vector<string>& words) {
 		Trie* const root = new Trie;
-		int total_length = static_cast<int>(words.size());
-		for (int i = 0; i < total_length; ++i)
-		{
+		const int word_num = static_cast<int>(words.size());
+		for (int i = 0; i < word_num; ++i) 
 			root->insert(words[i], i);
-		}
-
 
 		vector<vector<int>> result;
-		for (int i = 0; i < total_length; ++i)
-		{
-			const string& present_str = words[i];
+
+		for (int i = 0; i < word_num; ++i) {
+			auto s = words[i];
 			Trie* node = root;
-			for (int j = 0; j < present_str.size() && node != nullptr; ++j)
-			{
-				if (node->word_end_index != -1 &&
-					node->word_end_index != i &&
-					is_palindrome(present_str, j, present_str.size() - 1))
-				{
-					result.emplace_back(i, node->word_end_index);
-				}
-				node = node->next[present_str[j] - 'a'];
+			for (int j = 0; j < s.size() && node; ++j) {
+				if (node->end_index != -1 && node->end_index != i && isPalindrome(s, j, s.size() - 1)) result.push_back({ i, node->end_index });
+				// words[i]'s prefix matches this word and words[i]'s suffix forms a palindrome
+				node = node->next[s[j] - 'a'];
 			}
-			if (node == nullptr) continue;
-			for (const int index : node->palin_word)
-			{
-				if (index != i)
-				{
-					result.emplace_back(i, index);
-				}
+			if (!node) continue;
+			for (int j : node->palindromeIndexes) {
+				// words[i] is exhausted in the matching above. 
+				// If a word whose prefix is palindrome after matching its suffix with words[i], 
+				// then this is also a valid pair
+				if (i != j) result.push_back({ i, j });
 			}
 		}
-
 		return result;
 	}
 };
@@ -86,11 +61,10 @@ int main()
 	Solution solution;
 	vector<string> test_0{ "abcd", "dcba", "lls", "s", "sssll" };
 	vector<vector<int>> expect_0{ {0, 1}, { 1,0 }, { 3,2 }, { 2,4 } };
-	auto answer_0 = solution.palindromePairs(test_0);
+	vector<vector<int>> answer_0 = solution.palindromePairs(test_0);
 	expect(eq(answer_0.size(), expect_0.size()) >> fatal);
-	for (size_t i = 0; i < expect_0.size(); ++i)
+	for (const auto elem : expect_0)
 	{
-		expect(eq(answer_0[i], expect_0[i]));
+		expect(std::ranges::find(answer_0, elem) != answer_0.end());
 	}
-
 }
